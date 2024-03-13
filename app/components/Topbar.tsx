966df2d89081976a52b1cc4f5224008adf855de6
subashsivaneni18 <satyasaisubash8@gmail.com>
2024-03-13 16:20:15 +0530

    second

--- app/components/Topbar.tsx
+++ app/components/Topbar.tsx
@@ -14,7 +14,10 @@ const Topbar = () => {
   return (
     <div className="pt-10 flex space-x-3.5 gap-2 justify-center sm:gap-20 px-3">
       <>
-        <Link href="#schedule" className="flex items-center cursor-pointer">
+        <Link
+          href="#schedule"
+          className="flex items-center cursor-pointer dark:text-white text-black"
+        >
           <div className="flex items-center gap-0">
             <ChevronLeft />
             <ChevronRight />
@@ -22,12 +25,18 @@ const Topbar = () => {
           <p>The Schedule</p>
         </Link>
 
-        <Link href="#testimony" className="flex items-center cursor-pointer">
+        <Link
+          href="#testimony"
+          className="flex items-center cursor-pointer dark:text-white text-black"
+        >
           <User />
           <p>Testimonials</p>
         </Link>
 
-        <Link href="#FAQ" className="flex items-center cursor-pointer">
+        <Link
+          href="#FAQ"
+          className="flex items-center cursor-pointer dark:text-white text-black"
+        >
           <ShieldQuestion />
           <p>FAQ</p>
         </Link>
